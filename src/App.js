5012b60de73c88fd7c08e93335e3fef3d9d287d2
DylanPho <159408670+DylanPho@users.noreply.github.com>
2025-03-16 01:11:13 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import MovieHeader from './components/movieheader';
+import MovieList from './components/movielist';
+import Movie from './components/movie';
+import Authentication from './components/authentication';
+import {HashRouter, Routes,  Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter> {/* The Router component */}
+        <MovieHeader />
+        <Routes>
+          <Route path="/" element={<MovieList />} />
+          <Route path="/movielist" element={<MovieList />}/>
+          <Route path="/movie/:movieId" element={<Movie />}/>
+          <Route path="/signin" element={<Authentication />}/>
+          {/*... other routes */}
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
